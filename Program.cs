77955516cf23f570efae3dece4f7df4151585c36
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2025-09-30 14:31:42 +0200

    asd

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿#region pet list
-
 List<Pet> pets =
 [
     new()
@@ -92,6 +91,208 @@ List<Pet> pets =
         IsAlive = true,
         Legs = 1,
     },
+    new()
+    {
+        Name = "Fifike",
+        Owner = "Achukwu-Szirmai Patrick",
+        Species = "Dog",
+        Breed = "Dobberman",
+        Born = DateTime.Parse("2017-08-17"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Puki",
+        Owner = "Bárczi Dávid",
+        Species = "Chinchilla",
+        Breed = "Blue diamond",
+        Born = DateTime.Parse("2024-02-10"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Vénusz",
+        Owner = "Deák Márk",
+        Species = "Dog",
+        Breed = "Husky",
+        Born = DateTime.Parse("2024-08-10"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Hádész",
+        Owner = "Holzinger Vanessza",
+        Species = "Dog",
+        Breed = "Stuffy",
+        Born = DateTime.Parse("2023-04-16"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Lucie",
+        Owner = "Holzinger Vanessza",
+        Species = "Cat",
+        Breed = "Mixed breed",
+        Born = DateTime.Parse("2019-10-06"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Béla",
+        Owner = "Holzinger Vanessza",
+        Species = "Horse",
+        Breed = "Hungarian Sport Horse",
+        Born = DateTime.Parse("1996-05-06"),
+        IsAlive = false,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Kira",
+        Owner = "Holzinger Vanessza",
+        Species = "Dog",
+        Breed = "Labrador",
+        Born = DateTime.Parse("2022-03-16"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Nyafi",
+        Owner = "Iványi Bence",
+        Species = "Cat",
+        Breed = "Housecat",
+        Born = DateTime.Parse("2023-05-08"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Dobos",
+        Owner = "Kovács Albert",
+        Species = "Dog",
+        Breed = "Pitbull terrier",
+        Born = DateTime.Parse("2021-09-02"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Lucie",
+        Owner = "Paksa László",
+        Species = "Dog",
+        Breed = "Fench bulldog",
+        Born = DateTime.Parse("2019-11-01"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Akio",
+        Owner = "Szőke Máté",
+        Species = "Dog",
+        Breed = "Shiba-inu",
+        Born = DateTime.Parse("2018-12-10"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Cica",
+        Owner = "Tanai Gábor",
+        Species = "Cat",
+        Breed = "Calico",
+        Born = DateTime.Parse("2023-06-01"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Guszti",
+        Owner = "Tanai Gábor",
+        Species = "Cat",
+        Breed = "Housecat",
+        Born = DateTime.Parse("2024-07-01"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Drazsi",
+        Owner = "Turai Flóra",
+        Species = "Dog",
+        Breed = "Pug",
+        Born = DateTime.Parse("2019-01-11"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Fifike",
+        Owner = "Holzinger Vanessza",
+        Species = "Cat",
+        Breed = "Bengalian",
+        Born = DateTime.Parse("2022-12-04"),
+        IsAlive = true,
+        Legs = 3,
+    },
+    new()
+    {
+        Name = "Tücsi",
+        Owner = "Achukwu-Szirmai Patrick",
+        Species = "Dog",
+        Breed = "Dachshound",
+        Born = DateTime.Parse("2019-04-20"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Grabowsky",
+        Owner = "Juhász Zoltán",
+        Species = "Hamster",
+        Breed = "Robarowski dwarf hamster",
+        Born = DateTime.Parse("2020-05-02"),
+        IsAlive = false,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Mr. Wick",
+        Owner = "Juhász Zoltán",
+        Species = "Hamster",
+        Breed = "Dzungarian dwarf hamster",
+        Born = DateTime.Parse("2023-05-20"),
+        IsAlive = false,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Thomas Edison",
+        Owner = "Juhász Zoltán",
+        Species = "Dog",
+        Breed = "Streetmix",
+        Born = DateTime.Parse("2005-07-02"),
+        IsAlive = false,
+        Legs = 4,
+    },
+    new()
+    {
+        Name = "Tappancs",
+        Owner = "Juhász Zoltán",
+        Species = "Cat",
+        Breed = "housecat",
+        Born = DateTime.Parse("2000-12-04"),
+        IsAlive = false,
+        Legs = 4,
+    },
 ];
 
 #endregion
+
+Console.WriteLine($"count of pets: {pets.Count}");
