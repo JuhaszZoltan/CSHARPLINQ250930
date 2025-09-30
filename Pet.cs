fbae1e8ec4ef75bfb117ebae52ea9ddb3755d75d
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2025-09-30 13:28:17 +0200

    Add project files.

---
+++ Pet.cs
@@ -0,0 +1,13 @@
+﻿class Pet
+{
+    public string? Name { get; set; }
+    public string? Species { get; set; }
+    public string? Breed { get; set; }
+    public DateTime Born { get; set; }
+    public bool IsAlive { get; set; }
+    public float Age => (float)Math.Round(
+        (decimal)(DateTime.Now - Born).TotalDays / 365, 2);
+    public int Legs { get; set; }
+
+    public string? Owner { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿#region pet list
+
+List<Pet> pets =
+[
+    new()
+    {
+        Owner = "Varga Jázmin",
+        Species = "Dog",
+        Breed = "Vizsla",
+        Born = DateTime.Parse("2010-03-21"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Owner = "Szász Kristóf",
+        Species = "Cat",
+        Breed = "Persian",
+        Born = DateTime.Parse("2025-05-04"),
+        IsAlive = true,
+        Legs = 3,
+    },
+    new()
+    {
+        Owner = "Szalonnás István",
+        Species = "Dog",
+        Breed = "Westie",
+        Born = DateTime.Parse("2003-04-12"),
+        IsAlive = false,
+        Legs = 4,
+    },
+    new()
+    {
+        Owner = "Sajti Viktor",
+        Species = "Dog",
+        Breed = "Bishon havanese",
+        Born = DateTime.Parse("2019-05-02"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Owner = "Nagy Bálint",
+        Species = "Dog",
+        Breed = "Chiwawa",
+        Born = DateTime.Parse("2022-09-06"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Owner = "Kovács Ádám",
+        Species = "Fish",
+        Breed = "Sailfish",
+        Born = DateTime.Parse("2022-05-01"),
+        IsAlive = true,
+        Legs = 0,
+    },
+    new()
+    {
+        Owner = "Késmárki Gábor",
+        Species = "Spider",
+        Breed = "Tarantulla",
+        Born = DateTime.Parse("2025-09-30"),
+        IsAlive = true,
+        Legs = 8,
+    },
+    new()
+    {
+        Owner = "Herda Laura",
+        Species = "Snake",
+        Breed = "Corn snake",
+        Born = DateTime.Parse("2025-09-24"),
+        IsAlive = false,
+        Legs = 0,
+    },
+    new()
+    {
+        Owner = "Gaál Ámon",
+        Species = "Dog",
+        Breed = "Staffordshire terrier",
+        Born = DateTime.Parse("2015-12-10"),
+        IsAlive = true,
+        Legs = 4,
+    },
+    new()
+    {
+        Owner = "Ács Dániel",
+        Species = "Kangaroo",
+        Breed = "African kangaroo",
+        Born = DateTime.Parse("2016-08-17"),
+        IsAlive = true,
+        Legs = 1,
+    },
+];
+
+#endregion
